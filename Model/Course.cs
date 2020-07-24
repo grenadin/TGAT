7598bcbdbe16bfbaf8d3736c63c6b5f2cb85c68d
grenadin <harirak@hotmail.co.th>
2020-07-24 12:08:12 +0700

    Planning Model of TGAT

---
+++ Model/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TGAT.Model
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public int CourseTGATID { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Model/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TGAT.Model
+{
+    public class Student
+    {
+        public int ID { get; set; }
+       
+        public int StudentTGATId { get; set; }
+        public string Rank { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get => FirstName +" "+ LastName; }
+        public string FirstNameThai { get; set; }
+        public string LastNameThai { get; set; }
+        public string FullNameThai { get => FirstNameThai + " " + LastNameThai; }
+
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string License { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
